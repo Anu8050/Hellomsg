e8e4d03a086c29d444b4b6fa00c64d28f74897bd
Ausha M S <anusgowda050@gmail.com>
2023-03-07 12:21:16 +0530

    Final code cleanup

--- Hellomsg/Form1.Designer.cs
+++ Hellomsg/Form1.Designer.cs
@@ -28,46 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnclickthis = new System.Windows.Forms.Button();
-            this.lblHelloWorld = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // btnclickthis
-            // 
-            this.btnclickthis.Location = new System.Drawing.Point(310, 93);
-            this.btnclickthis.Name = "btnclickthis";
-            this.btnclickthis.Size = new System.Drawing.Size(91, 35);
-            this.btnclickthis.TabIndex = 0;
-            this.btnclickthis.Text = "Click This";
-            this.btnclickthis.UseVisualStyleBackColor = true;
-            this.btnclickthis.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // lblHelloWorld
-            // 
-            this.lblHelloWorld.AutoSize = true;
-            this.lblHelloWorld.Location = new System.Drawing.Point(449, 232);
-            this.lblHelloWorld.Name = "lblHelloWorld";
-            this.lblHelloWorld.Size = new System.Drawing.Size(143, 20);
-            this.lblHelloWorld.TabIndex = 1;
-            this.lblHelloWorld.Text = "Python file Contant";
-            this.lblHelloWorld.Click += new System.EventHandler(this.label1_Click);
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(318, 101);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
             // button2
             // 
             this.button2.BackColor = System.Drawing.SystemColors.MenuHighlight;
-            this.button2.Location = new System.Drawing.Point(614, 73);
+            this.button2.Location = new System.Drawing.Point(261, 77);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(174, 51);
             this.button2.TabIndex = 3;
@@ -81,21 +48,13 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.lblHelloWorld);
-            this.Controls.Add(this.btnclickthis);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button btnclickthis;
-        private System.Windows.Forms.Label lblHelloWorld;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
     }
 }
--- Hellomsg/Form1.cs
+++ Hellomsg/Form1.cs
@@ -17,29 +17,6 @@ namespace Hellomsg
         {
             InitializeComponent();
         }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            //lblHelloWorld.Text = "hello";
-            ProcessStartInfo start = new ProcessStartInfo();
-            start.FileName = @"C:\Users\User\AppData\Local\Programs\Python\Python310\python.exe";
-            start.Arguments = @"F:\\C#Example\\PythonFile\\print.py";
-            start.UseShellExecute = false;
-            start.CreateNoWindow = true;
-            start.RedirectStandardOutput = true;
-            string result = "";
-            using (Process process = Process.Start(start))
-            {
-                result = process.StandardOutput.ReadToEnd();
-            }
-            lblHelloWorld.Text = result;
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
         private void button2_Click(object sender, EventArgs e)
         {
 
@@ -61,9 +38,5 @@ namespace Hellomsg
 
         }
 
-        private void button1_Click_1(object sender, EventArgs e)
-        {
-
-        }
     }
 }
