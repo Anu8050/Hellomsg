a10e0c14f39e0c194d37d10f33793e8bac0005d6
Ausha M S <anusgowda050@gmail.com>
2023-03-07 18:53:28 +0530

    .

--- Hellomsg/Program.cs
+++ Hellomsg/Program.cs
@@ -17,6 +17,7 @@ namespace Hellomsg
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+           
         }
     }
 }
