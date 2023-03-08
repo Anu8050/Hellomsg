f6d2d0226935e1332c6e59b92146c9043ef73139
Ausha M S <anusgowda050@gmail.com>
2023-03-08 19:29:48 +0530

    Check minimum 2 files are selected or not.

--- Hellomsg/Form1.cs
+++ Hellomsg/Form1.cs
@@ -3,6 +3,7 @@ using IronPython.Hosting;
 using Microsoft.Scripting.Hosting;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace MergeFilesTool
@@ -20,9 +21,10 @@ namespace MergeFilesTool
         {
             OpenFileDialog fdlg = new OpenFileDialog();
             fdlg.Title = "Select first pdf file";
-            fdlg.InitialDirectory = @"f:\";
+            fdlg.InitialDirectory = @"C:\Users\User\Documents\";
             //Only allow pdf files
-            fdlg.Filter = "All files (*.*)|*.*|All files (*.*)|*.*";
+            fdlg.Filter = "Pdf Files (.pdf)|*.pdf";
+                
             fdlg.FilterIndex = 2;
             fdlg.RestoreDirectory = true;
             if (fdlg.ShowDialog() == DialogResult.OK)
@@ -36,9 +38,9 @@ namespace MergeFilesTool
             OpenFileDialog fdlg = new OpenFileDialog();
             fdlg.Title = "Select second pdf file";
             //point to documents folder
-            fdlg.InitialDirectory = @"f:\";
+            fdlg.InitialDirectory = @"C:\Users\User\Documents\";
             //Only allow pdf files
-            fdlg.Filter = "All files (*.*)|*.*|All files (*.*)|*.*";
+            fdlg.Filter = "Pdf Files (.pdf)|*.pdf";
             fdlg.FilterIndex = 2;
             fdlg.RestoreDirectory = true;
             if (fdlg.ShowDialog() == DialogResult.OK)
@@ -50,9 +52,9 @@ namespace MergeFilesTool
         private void button4_Click(object sender, EventArgs e)
         {
             OpenFileDialog fdlg = new OpenFileDialog();
-            fdlg.Title = "F# Corner Open File Dialog";
-            fdlg.InitialDirectory = @"f:\";
-            fdlg.Filter = "All files (*.*)|*.*|All files (*.*)|*.*";
+            fdlg.Title = "Select third pdf file";
+            fdlg.InitialDirectory = @"C:\Users\User\Documents\";
+            fdlg.Filter = "Pdf Files (.pdf)|*.pdf";
             fdlg.FilterIndex = 2;
             fdlg.RestoreDirectory = true;
             if (fdlg.ShowDialog() == DialogResult.OK)
@@ -63,23 +65,47 @@ namespace MergeFilesTool
 
         private void button5_Click(object sender, EventArgs e)
         {
+            
 
             textboxs = new List<string>
             {
                 txtFirstFile.Text, txtSecondFile.Text,txtThirdFile.Text
             };
 
+            if((textboxs.ElementAt(0).Length != 0 && textboxs.ElementAt(1).Length != 0 && textboxs.ElementAt(2).Length != 0)
+                || (textboxs.ElementAt(0).Length == 0 && textboxs.ElementAt(1).Length != 0 && textboxs.ElementAt(2).Length != 0)
+                || (textboxs.ElementAt(0).Length != 0 && textboxs.ElementAt(1).Length == 0 && textboxs.ElementAt(2).Length != 0)
+                || (textboxs.ElementAt(0).Length != 0 && textboxs.ElementAt(1).Length != 0 && textboxs.ElementAt(2).Length == 0)
+
+                )
+            {
+                ScriptEngine engine = Python.CreateEngine();
+                ScriptScope scope = engine.CreateScope();
+
+                engine.ExecuteFile(Environment.CurrentDirectory + @"\pythonscript\mergefiles.py", scope);
+                dynamic sumFunction = scope.GetVariable("arry1");
+                var result = sumFunction(textboxs);
+                lblStatus.Text = result;
+
+            }
+            else 
+            {
+                MessageBox.Show("Select minimum two pdf file");
+            }
+
+            
+
             //textboxs.Add(textBox2.Text);
             //textboxs.Add(textBox3.Text);
             //textboxs.Add(textBox4.Text);
 
-            ScriptEngine engine = Python.CreateEngine();
-            ScriptScope scope = engine.CreateScope();
+            //ScriptEngine engine = Python.CreateEngine();
+            //ScriptScope scope = engine.CreateScope();
 
-            engine.ExecuteFile(Environment.CurrentDirectory + @"\pythonscript\mergefiles.py", scope);
-            dynamic sumFunction = scope.GetVariable("arry1");
-            var result = sumFunction(textboxs);
-            lblStatus.Text = result;
+            //engine.ExecuteFile(Environment.CurrentDirectory + @"\pythonscript\mergefiles.py", scope);
+            //dynamic sumFunction = scope.GetVariable("arry1");
+            //var result = sumFunction(textboxs);
+            //lblStatus.Text = result;
 
 
             //ScriptRuntimeSetup setup = Python.CreateRuntimeSetup(null);
