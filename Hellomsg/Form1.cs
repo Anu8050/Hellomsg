08b6aa34d4871c6d83028317b90d60ad6a5455ea
Ausha M S <anusgowda050@gmail.com>
2023-03-29 10:35:54 +0530

    Removing Unused lines of code.

--- Hellomsg/Form1.cs
+++ Hellomsg/Form1.cs
@@ -1,12 +1,9 @@
 ﻿
-using IronPython.Hosting;
-using javax.script;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
 using ceTe.DynamicPDF.Merger;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 
 namespace MergeFilesTool
@@ -14,7 +11,6 @@ namespace MergeFilesTool
     public partial class Form1 : Form
     {
         List<string> textboxs = new List<string>();
-        //string[] textboxs = new string[5];
         public Form1()
         {
             InitializeComponent();
@@ -38,7 +34,7 @@ namespace MergeFilesTool
         private void button3_Click_1(object sender, EventArgs e)
         {
             OpenFileDialog fdlg = new OpenFileDialog();
-            fdlg.Title = "Select second pdf file";
+            fdlg.Title = "Select second pdf file.";
             //point to documents folder
             fdlg.InitialDirectory = @"C:\Users\User\Documents\";
             //Only allow pdf files
@@ -54,7 +50,7 @@ namespace MergeFilesTool
         private void button4_Click(object sender, EventArgs e)
         {
             OpenFileDialog fdlg = new OpenFileDialog();
-            fdlg.Title = "Select third pdf file";
+            fdlg.Title = "Select third pdf file.";
             fdlg.InitialDirectory = @"C:\Users\User\Documents\";
             fdlg.Filter = "Pdf Files (.pdf)|*.pdf";
             fdlg.FilterIndex = 2;
@@ -67,12 +63,6 @@ namespace MergeFilesTool
 
         private void button5_Click(object sender, EventArgs e)
         {
-
-            //textboxs = new List<string>
-            //{
-            //    txtFirstFile.Text, txtSecondFile.Text,txtThirdFile.Text
-            //};
-
             textboxs.Add(txtFirstFile.Text);
             textboxs.Add(txtSecondFile.Text);
             textboxs.Add(txtThirdFile.Text);
@@ -85,35 +75,38 @@ namespace MergeFilesTool
                     (textboxs.ElementAt(1).Length != 0) && 
                     (textboxs.ElementAt(2).Length != 0))
                 {
-                    MessageBox.Show("Sucessfuly merge 3 pdf files.");
+                    MessageBox.Show("Sucessfuly merge First, second and Thrid pdf files.");
                     MergeDocument document = new MergeDocument(txtFirstFile.Text);
                     document.Append(txtSecondFile.Text);
                     document.Append(txtThirdFile.Text);
                     document.Draw("C://Users//User//Documents//Anuoutput2.pdf");
                 }
+
                 else if ((textboxs.ElementAt(0).Length == 0 && 
                     textboxs.ElementAt(1).Length != 0 && 
                     textboxs.ElementAt(2).Length != 0))
                 {
-                    MessageBox.Show("Sucessfuly merge 3 pdf files.");
+                    MessageBox.Show("Sucessfuly merge second and thrid pdf files.");
                     MergeDocument document = new MergeDocument(txtSecondFile.Text);;
                     document.Append(txtThirdFile.Text);
                     document.Draw("C://Users//User//Documents//Anuoutput2.pdf");
                 }
+
                 else if ((textboxs.ElementAt(0).Length != 0 && 
                     textboxs.ElementAt(1).Length == 0 && 
                     textboxs.ElementAt(2).Length != 0))
                 {
-                    MessageBox.Show("Sucessfuly merge 3 pdf files.");
+                    MessageBox.Show("Sucessfuly merge"+ txtFirstFile.Text + " and"+ txtThirdFile.Text + " pdf files.");
                     MergeDocument document = new MergeDocument(txtFirstFile.Text); ;
                     document.Append(txtThirdFile.Text);
                     document.Draw("C://Users//User//Documents//Anuoutput2.pdf");
                 }
+
                 else if ((textboxs.ElementAt(0).Length != 0 && 
                     textboxs.ElementAt(1).Length != 0 && 
                     textboxs.ElementAt(2).Length == 0))
                 {
-                    MessageBox.Show("Sucessfuly merge 3 pdf files.");
+                    MessageBox.Show("Sucessfuly merge "+ txtFirstFile.Text +" and"+ txtSecondFile.Text + " pdf files.");
                     MergeDocument document = new MergeDocument(txtFirstFile.Text); ;
                     document.Append(txtSecondFile.Text);
                     document.Draw("C://Users//User//Documents//Anuoutput2.pdf");
@@ -124,13 +117,8 @@ namespace MergeFilesTool
                     MessageBox.Show("Select minimum two pdf files");
                 }
 
-
             }
-            
-
-            
+   
         }
-
     }
-}
-;
+};
