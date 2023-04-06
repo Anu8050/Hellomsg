9eddea7775e94c1617b26ba03ea8afe6441fc5d3
Ausha M S <anusgowda050@gmail.com>
2023-04-06 13:40:21 +0530

    Retrying merge pdf function.

--- Hellomsg/Form1.cs
+++ Hellomsg/Form1.cs
@@ -3,13 +3,8 @@ using System.Linq;
 using System;
 using System.Windows.Forms;
 using IronPython.Hosting;
-using IronPython.Runtime;
 using Microsoft.Scripting.Hosting;
-
-//using IronPython.Runtime;
-//using IronPython;
-//using Microsoft.Scripting;
-
+using java.nio.file;
 
 namespace MergeFilesTool
 {
@@ -80,71 +75,28 @@ namespace MergeFilesTool
                     (textboxs.ElementAt(1).Length != 0) && 
                     (textboxs.ElementAt(2).Length != 0))
                 {
+                    // Microsoft.Scripting.Hosting.ScriptEngine engine = Python.CreateEngine();
+
                     ScriptEngine engine = Python.CreateEngine();
                     ScriptScope scope = engine.CreateScope();
                     var paths = engine.GetSearchPaths();
-
-                    //paths.Add(@"C:\\Users\\User\\AppData\\Local\\Programs\\Python\\Python310\\Scripts");
-                    //paths.Add(@"C:\\Users\\User\\AppData\Local\\Programs\\Python\\Python310\\include");
-                    //paths.Add(@"C:\\Users\\User\\AppData\\Local\\Programs\\Python\Python310\\Lib");
-                    paths.Add(@"C:\Users\User\AppData\Local\Programs\Python\Python310\Lib\site-packages");
                     paths.Add(@"C:\Users\User\AppData\Local\Programs\Python\Python310\Lib\site-packages\PyPDF2");
-                    paths.Add(@"C:\\Program Files\\IronPython 3.4\\Lib");
-                    paths.Add(@"F:\C#Example\Hellomsg\Hellomsg\packages\IronPython.3.4.0");
-                    paths.Add(@"F:\C#Example\Hellomsg\Hellomsg\packages\IronPython.StdLib.3.4.0");
-
-
-                    //paths.Add(@"F:\C#Example\Hellomsg\Hellomsg\packages\DynamicLanguageRuntime.1.3.3");
-                    //paths.Add(@"C:\Users\User\AppData\Local\Programs\Python\Python310\Scripts");
-                    //paths.Add(@"C:\Users\User\AppData\Local\Programs\Python\Python310\include");
-                    //paths.Add(@"C:\Users\User\AppData\Local\Programs\Python\Python310\Lib");
-                    //paths.Add(@"C:\Users\User\AppData\Local\Programs\Python\Python310\Lib\site-packages\PyPDF2");
-                    ////paths.Add(@"F:\C#Example\Hellomsg\Hellomsg");
-                    //paths.Add(@"C:\Program Files\IronPython 2.7\Lib");
-
-
-                    //paths.Add(@"C:\myProject\'where mergefiles.py exists'");
-
-                    //engine.SetSearchPaths(paths);
-                    //var mainfile = @"F:\C#Example\Hellomsg\Hellomsg\pythonscript\mergefiles.py";
-                    ////var scope = engine.CreateScope();
-                    //engine.CreateScriptSourceFromFile(mainfile).Execute(scope);
-                    //var result = scope.GetVariable("merge1");
-                    //// Console.WriteLine(result);
-                    //Console.ReadKey();
-
-                    //paths.Add(@"C:\\Users\\User\\AppData\\Local\\Programs\\Python\\Python310\\DLLs");
-                    //paths.Add(@"C:\\Users\\User\\AppData\\Local\\Programs\\Python\\Python310\\lib");
-                    //paths.Add(@"C:\\Users\\User\\AppData\\Local\\Programs\\Python\\Python310");
-                    //paths.Add(@"C:\\Users\\User\\AppData\\Roaming\\Python\\Python310\\site-packages");
-                    //paths.Add(@"C:\\Users\\User\\AppData\\Local\\Programs\\Python\\Python310\\Lib\\site-packages\\PyPDF2");
-                    //engine.SetSearchPaths(paths);
-                    //engine.ExecuteFile(@"C:\Users\User\Documents\mergefiles.py", scope); 
-
-                    //engine.ExecuteFile(@"F:\C#Example\Hellomsg\Hellomsg\pythonscript\mergefiles.py", scope);
-                    //engine.ExecuteFile(Environment.CurrentDirectory + @"\pythonscript\mergefiles.py", scope);
-                    engine.ExecuteFile(Environment.CurrentDirectory + @"\pythonscript\mergefiles.py", scope);
                     engine.SetSearchPaths(paths);
-                    dynamic sumFunction = scope.GetVariable("merge");
+                    engine.ExecuteFile(Environment.CurrentDirectory + @"\pythonscript\mergefiles.py", scope);
+                    dynamic sumFunction = scope.GetVariable("merge1");
                     var result = sumFunction(textboxs);
                     lblStatus.Text = result;
+
                 }
 
                 else if ((textboxs.ElementAt(0).Length == 0 && 
                     textboxs.ElementAt(1).Length != 0 && 
                     textboxs.ElementAt(2).Length != 0))
                 {
-                    ScriptEngine engine = Python.CreateEngine();
-                    ScriptScope scope = engine.CreateScope();
-                    engine.ExecuteFile(Environment.CurrentDirectory + @"\pythonscript\mergefiles.py", scope);
-                    dynamic sumFunction = scope.GetVariable("merge");
-                    var result = sumFunction(textboxs);
-                    lblStatus.Text = result;
-                    MessageBox.Show("Sucessfuly merge second and thrid pdf files.");   
+                     
                 }
 
                 else if ((textboxs.ElementAt(0).Length != 0 && 
-                    textboxs.ElementAt(1).Length == 0 && 
                     textboxs.ElementAt(2).Length != 0))
                 {  
                     MessageBox.Show("Sucessfuly merge"+ txtFirstFile.Text + " and"+ txtThirdFile.Text + " pdf files.");  
--- Hellomsg/pythonscript/mergefiles.py
+++ Hellomsg/pythonscript/mergefiles.py
@@ -1,23 +1,13 @@
-#import datetime
-#import os
-
-
-#def merge1(pdfiles):
- #   for i in pdfiles:
- #       print(i)
- #       x = datetime.datetime.now()
- #       print(x)
- #   return "Anu"
-
-#import aspose.words as aw
-
-import sys
-print(sys.version)
-sys.path.append(r'C:\Users\User\AppData\Local\Programs\Python\Python310\Lib\site-packages\PyPDF2')
-
-import PyPDF2
+import imp
+PyPDF2 =imp.load_source('PyPDF2', 'C:\Users\User\AppData\Local\Programs\Python\Python310\Lib\site-packages\PyPDF2\__init__.py')
 import os
 
+def merge(pdfiles):
+    print(pdfiles)
+    return "hi"
+
+import sys; 
+print(sys.path)
 
 pdfMerge = PyPDF2.PdfMerger()
 pdfiles = []
@@ -26,17 +16,17 @@ pdfiles = []
      #   'C:/Users/User/Documents/Anusha.pdf',
       #'C:/Users/User/Documents/pdf_files/sample.pdf'
        # }
-def merge(pdfiles):
+def merge1(pdfiles):
     for filename in pdfiles:
             pdfFile = open(filename, 'rb')
             pdf_reader = PyPDF2.PdfReader(pdfFile)
             pdfMerge.append(pdf_reader)
     pdfFile.close()
-    pdfMerge.write('C://Users//User//Documents//pdf_files//merged.pdf')
+    pdfMerge.write('C://Users//User//Documents//pdf_files//merged2.pdf')
     return "Sucessfuly merge pdf files."
-
+    
 
 
 
 if __name__ == "__main__":
-   merge(pdfiles)
+   merge1(pdfiles)
