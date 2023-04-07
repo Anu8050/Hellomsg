b56e5d3b169312ba14f15381ea27d7c53c987baf
Ausha M S <anusgowda050@gmail.com>
2023-04-07 16:58:52 +0530

    Updating mergefiles.py file.

--- Hellomsg/pythonscript/mergefiles.py
+++ Hellomsg/pythonscript/mergefiles.py
@@ -1,32 +1,33 @@
-#import clr
-#clr.AddReference('System')
-#from System import *
 import sys
-#print(sys.version)
-#sys.path.append(r'C:\Users\User\AppData\Local\Programs\Python\Python310\Lib')
-sys.path.append(r"C:\\Program Files\\IronPython 3.4\\Lib");
-
+print(sys.version)
 import PyPDF2
-import os
+sys.path.append("C:\Program Files\IronPython 3.4\Lib")
+print(sys.path)
+
+
+def merge(pdfiles):
+    print(pdfiles)
+    return "hi"
+
 
 
 pdfMerge = PyPDF2.PdfMerger()
-pdfiles = ['C:/Users/User/Documents/pdf_files/a.pdf',
-        'C:/Users/User/Documents/Anusha.pdf',]
+pdfiles = ['C:/Users/User/Documents/pdf_files/sample.pdf','C:/Users/User/Documents/pdf_files/sample.pdf']
 #pdfiles ={
  #       'C:/Users/User/Documents/pdf_files/a.pdf',
      #   'C:/Users/User/Documents/Anusha.pdf',
       #'C:/Users/User/Documents/pdf_files/sample.pdf'
        # }
-def merge(pdfiles):
+def merge1(pdfiles):
     for filename in pdfiles:
             pdfFile = open(filename, 'rb')
             pdf_reader = PyPDF2.PdfReader(pdfFile)
             pdfMerge.append(pdf_reader)
     pdfFile.close()
-    pdfMerge.write('C:\\Users\\User\\Documents\\pdf_files\\merged3.pdf')
+    pdfMerge.write('C://Users//User//Documents//pdf_files//merged2.pdf')
+    print("Sucessfuly merge pdf files.")
     return "Sucessfuly merge pdf files."
-
+    
 
 if __name__ == "__main__":
-   merge(pdfiles)
+   merge1(pdfiles)
