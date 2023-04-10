afdb1c19f7b27a20ea1b19eeaff39bdd1b00e936
Ausha M S <anusgowda050@gmail.com>
2023-04-10 18:53:45 +0530

    Final working copy(deployment).

--- Hellomsg/Form1.cs
+++ Hellomsg/Form1.cs
@@ -120,7 +120,7 @@ namespace MergeFilesTool
                                      };
 
                     engine.SetSearchPaths(libs);
-                    engine.ExecuteFile(Environment.CurrentDirectory + @"\pythonscript\mergefiles.py", scope);
+                    engine.ExecuteFile(Environment.CurrentDirectory + @"\pythonscript\hello.py", scope);
                     dynamic sumFunction = scope.GetVariable("merge1");
                     for (int i = textboxs.Count - 1; i >= 0; i--)
                     {
@@ -153,7 +153,7 @@ namespace MergeFilesTool
                                      };
 
                     engine.SetSearchPaths(libs);
-                    engine.ExecuteFile(Environment.CurrentDirectory + @"\pythonscript\mergefiles.py", scope);
+                    engine.ExecuteFile(Environment.CurrentDirectory + @"\pythonscript\hello.py", scope);
                     dynamic sumFunction = scope.GetVariable("merge1");
                     for (int i = textboxs.Count - 1; i >= 0; i--)
                     {
@@ -187,7 +187,7 @@ namespace MergeFilesTool
                                      };
 
                     engine.SetSearchPaths(libs); 
-                    engine.ExecuteFile(Environment.CurrentDirectory + @"\pythonscript\mergefiles.py", scope);
+                    engine.ExecuteFile(Environment.CurrentDirectory + @"\pythonscript\hello.py", scope);
                     dynamic sumFunction = scope.GetVariable("merge1");
                     for (int i = textboxs.Count - 1; i >= 0; i--)
                     {
