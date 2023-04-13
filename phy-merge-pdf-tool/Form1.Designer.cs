ae57689b401882323511779b5c72a4fefa4b600e
Ausha M S <anusgowda050@gmail.com>
2023-04-13 18:51:27 +0530

    Adding latest code(13th)

---
+++ phy-merge-pdf-tool/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿using System;
+
+namespace phy_merge_pdf_tool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtFirstFile = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.txtSecondFile = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.txtThirdFile = new System.Windows.Forms.TextBox();
+            this.btnMergePdfFiles = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtmergefilename = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button2.Location = new System.Drawing.Point(576, 55);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(106, 40);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Browse";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtFirstFile
+            // 
+            this.txtFirstFile.Location = new System.Drawing.Point(46, 63);
+            this.txtFirstFile.Name = "txtFirstFile";
+            this.txtFirstFile.Size = new System.Drawing.Size(506, 26);
+            this.txtFirstFile.TabIndex = 6;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button3.Location = new System.Drawing.Point(576, 120);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(106, 40);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Bowse";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // txtSecondFile
+            // 
+            this.txtSecondFile.Location = new System.Drawing.Point(46, 128);
+            this.txtSecondFile.Name = "txtSecondFile";
+            this.txtSecondFile.Size = new System.Drawing.Size(506, 26);
+            this.txtSecondFile.TabIndex = 8;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button4.Location = new System.Drawing.Point(576, 185);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(106, 40);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Browse";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // txtThirdFile
+            // 
+            this.txtThirdFile.Location = new System.Drawing.Point(46, 191);
+            this.txtThirdFile.Name = "txtThirdFile";
+            this.txtThirdFile.Size = new System.Drawing.Size(506, 26);
+            this.txtThirdFile.TabIndex = 10;
+            // 
+            // btnMergePdfFiles
+            // 
+            this.btnMergePdfFiles.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.btnMergePdfFiles.Location = new System.Drawing.Point(645, 417);
+            this.btnMergePdfFiles.Name = "btnMergePdfFiles";
+            this.btnMergePdfFiles.Size = new System.Drawing.Size(134, 60);
+            this.btnMergePdfFiles.TabIndex = 11;
+            this.btnMergePdfFiles.Text = "Merge pdf files";
+            this.btnMergePdfFiles.UseVisualStyleBackColor = false;
+            this.btnMergePdfFiles.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(14, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(141, 20);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Merge Files Tool";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(46, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(135, 20);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Select first pdf file";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(46, 105);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(161, 20);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Select second pdf file";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(46, 168);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(140, 20);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Select third pdf file";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtmergefilename);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtFirstFile);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.txtSecondFile);
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.txtThirdFile);
+            this.groupBox1.Location = new System.Drawing.Point(16, 78);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(762, 318);
+            this.groupBox1.TabIndex = 16;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Browse files to merge";
+            // 
+            // txtmergefilename
+            // 
+            this.txtmergefilename.Location = new System.Drawing.Point(46, 248);
+            this.txtmergefilename.Name = "txtmergefilename";
+            this.txtmergefilename.Size = new System.Drawing.Size(506, 26);
+            this.txtmergefilename.TabIndex = 19;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(46, 225);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(131, 20);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "Merged file name";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(68, 377);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(0, 20);
+            this.lblStatus.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1052, 522);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnMergePdfFiles);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "phyelements pdf merge tool";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtFirstFile;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox txtSecondFile;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox txtThirdFile;
+        private System.Windows.Forms.Button btnMergePdfFiles;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtmergefilename;
+    }
+}
+
