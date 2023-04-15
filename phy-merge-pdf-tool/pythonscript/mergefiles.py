8b9804865a6133eb45938e30067e62aa844017ea
Ausha M S <anusgowda050@gmail.com>
2023-04-15 13:55:12 +0530

    Adding returning statement to file present or not.

--- phy-merge-pdf-tool/pythonscript/mergefiles.py
+++ phy-merge-pdf-tool/pythonscript/mergefiles.py
@@ -12,25 +12,25 @@ def merge1(pdfiles, mergepdf_filename):
         pdfMerge.append(pdf_reader)
         pdfFile.close()
     
-
-    if (directory + mergepdf_filename +'.pdf') == os.path.isfile(directory + mergepdf_filename +'.pdf'):
-        print("true")
-
+    ched = os.path.isfile(directory + mergepdf_filename +'.pdf')
+    print(ched)
+    if ched != True:
+        #print(mergepdf_filename)
         with open(directory + mergepdf_filename +'.pdf', 'wb') as mergedFile:
             pdfMerge.write(mergedFile)
-    
-    else: 
-        print("false")
+    else:
+        #print("file is already exist.")
+        return "File is already present in "+ directory+ "please enetr another name."
 
     #C://Users//User//Documents// +{mergepdf_filename} +.pdf
     print("Successfully merged pdf files.")
     #return "Merged file is created in " + directory + mergepdf_filename +'.pdf';
 
 directory = "C://Users//User//Documents//"
-pdfiles=[]
-mergepdf_filename = ''
-#mergepdf_filename='anu1'
-#pdfiles=['C:/Users/User/Documents/Anusha.pdf','C:/Users/User/Documents/Anusha.pdf']
+#pdfiles=[]
+#mergepdf_filename = ''
+mergepdf_filename='vv3'
+pdfiles=['C:/Users/User/Documents/Anusha.pdf','C:/Users/User/Documents/Anusha.pdf']
 if __name__ == "__main__":
    merge1(pdfiles, mergepdf_filename)
 
